490f8a30c6aea733884a1c4501d7f83525f33d57
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-27 20:04:46 +0300

    feat: сверстать header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,27 +7,28 @@ function Header(props) {
   return (
     <header className="header">
       <div className="header__container">
-      <Link to="/" className="header__logo-link">
-        <img className="header__logo" src={headerLogo} alt="Логотип в виде текста NewsExplorer." />
-      </Link>
+        <Link to="/" className="header__logo-link tap-highlight">
+          <img className="header__logo" src={headerLogo} alt="Логотип в виде текста NewsExplorer." />
+        </Link>
         {props.loggedIn
-          ? (<nav>
-              <Link to="/" className="header__link header__text opacity tab">Главная</Link>
-              <Link to="/saved-news" className="header__link header__text opacity tab">Сохранённые статьи</Link>
-              <button className="header__nav-button header__nav-button_signout header__text opacity tab" type="button" onClick={props.onSignOut}>Грета</button>
+          ? (<nav className="header__menu">
+              <Link to="/" className="header__link header__text opacity tab tap-highlight">Главная</Link>
+              <Link to="/saved-news" className="header__link header__text opacity tab tap-highlight">Сохранённые статьи</Link>
+              <button className="header__nav-button header__nav-button_signout header__text opacity tab tap-highlight" type="button" onClick={props.onSignOut}>Грета</button>
             </nav>)
 
-          : (<nav>
-              <Link to="/" className="header__link header__text opacity tab">Главная</Link>
-              <button className="header__nav-button header__nav-button_signin header__text opacity tab" type="button">Авторизоваться</button>
+          : (<nav className="header__menu">
+              <Link to="/" className="header__link header__text opacity tab tap-highlight">Главная</Link>
+              <button className="header__nav-button header__nav-button_signin header__text opacity tab tap-highlight" type="button">Авторизоваться</button>
             </nav>)
         }
+        <button className="header__mobile-button tap-highlight" type="button" aria-label="Открыть меню."></button>
       </div>
       <h1 className="header__title">Что творится в&nbsp;мире?</h1>
       <p className="header__subtitle">Находите самые свежие статьи на&nbsp;любую тему и&nbsp;сохраняйте в&nbsp;своём личном кабинете.</p>
       <form className="header__form">
         <input className="header__input" placeholder="Введите тему новости" />
-        <button className="header__search-button header__text" type="submit">Искать</button>
+        <button className="header__search-button header__text tap-highlight" type="submit">Искать</button>
       </form>
     </header>
   );

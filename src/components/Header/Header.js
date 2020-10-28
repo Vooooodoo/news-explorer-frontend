7366b81a2197e6e8b68a47f9a2489150b147d4b0
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-28 19:13:35 +0300

    feat: сверстать header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import headerLogo from '../../images/header__logo.svg';
+import Navigation from '../Navigation/Navigation';
+import SearchForm from '../SearchForm/SearchForm';
 import './Header.css';
 
 function Header(props) {
   return (
     <header className="header">
-      <div className="header__container">
-        <Link to="/" className="header__logo-link tap-highlight">
-          <img className="header__logo" src={headerLogo} alt="Логотип в виде текста NewsExplorer." />
-        </Link>
-        {props.loggedIn
-          ? (<nav className="header__menu">
-              <Link to="/" className="header__link header__text opacity tab tap-highlight">Главная</Link>
-              <Link to="/saved-news" className="header__link header__text opacity tab tap-highlight">Сохранённые статьи</Link>
-              <button className="header__nav-button header__nav-button_signout header__text opacity tab tap-highlight" type="button" onClick={props.onSignOut}>Грета</button>
-            </nav>)
-
-          : (<nav className="header__menu">
-              <Link to="/" className="header__link header__text opacity tab tap-highlight">Главная</Link>
-              <button className="header__nav-button header__nav-button_signin header__text opacity tab tap-highlight" type="button">Авторизоваться</button>
-            </nav>)
-        }
-        <button className="header__mobile-button tap-highlight" type="button" aria-label="Открыть меню."></button>
-      </div>
+      <Navigation
+        loggedIn={props.loggedIn}
+        onSignOut={props.onSignOut}
+      />
       <h1 className="header__title">Что творится в&nbsp;мире?</h1>
       <p className="header__subtitle">Находите самые свежие статьи на&nbsp;любую тему и&nbsp;сохраняйте в&nbsp;своём личном кабинете.</p>
-      <form className="header__form">
-        <input className="header__input" placeholder="Введите тему новости" />
-        <button className="header__search-button header__text tap-highlight" type="submit">Искать</button>
-      </form>
+      <SearchForm />
     </header>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import headerLogo from '../../images/header__logo.svg';
+import './Navigation.css';
+
+function Navigation(props) {
+  return (
+    <div className="navigation">
+      <Link to="/" className="navigation__logo-link tap-highlight">
+        <img className="navigation__logo" src={headerLogo} alt="Логотип в виде текста NewsExplorer." />
+      </Link>
+      {props.loggedIn
+        ? (<nav className="navigation__menu">
+            <Link to="/" className="navigation__link navigation__text opacity tab tap-highlight">Главная</Link>
+            <Link to="/saved-news" className="navigation__link navigation__text opacity tab tap-highlight">Сохранённые статьи</Link>
+            <button className="navigation__desktop-button navigation__desktop-button_signout navigation__text opacity tab tap-highlight" type="button" onClick={props.onSignOut}>Грета</button>
+          </nav>)
+
+        : (<nav className="navigation__menu">
+            <Link to="/" className="navigation__link navigation__text opacity tab tap-highlight">Главная</Link>
+            <button className="navigation__desktop-button navigation__desktop-button_signin navigation__text opacity tab tap-highlight" type="button">Авторизоваться</button>
+          </nav>)
+      }
+      <button className="navigation__mobile-button tap-highlight" type="button" aria-label="Открыть меню."></button>
+    </div>
+  );
+}
+
+export default Navigation;

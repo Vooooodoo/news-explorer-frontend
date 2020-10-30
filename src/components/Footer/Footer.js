250e1fe0c04b17901ef74d7df52a3cde1ecb9248
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-30 18:59:55 +0300

    feat: сверстать footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">&copy;&nbsp;2020 NewsExplorer, Powered by News API</p>
-      <nav className="footer__container">
+      <nav className="footer__navigation">
         <div>
           <Link to="/" className="footer__link opacity tab tap-highlight">Главная</Link>
           <a className="footer__link opacity tab tap-highlight" href="https://praktikum.yandex.ru" target="_blank">Яндекс.Практикум</a>
         </div>
         <div>
-          <a className="opacity tab tap-highlight" href="https://github.com/Vooooodoo" target="_blank">
-            <img className="footer__logo" src={githubLogo} alt="Логотип Гитхаб." />
+          <a className="footer__logo-link opacity tab tap-highlight" href="https://github.com/Vooooodoo" target="_blank">
+            <img className="footer__logo footer__logo_type_github" src={githubLogo} alt="Логотип Гитхаб." />
           </a>
-          <a className="opacity tab tap-highlight" href="https://www.facebook.com/" target="_blank">
-            <img className="footer__logo" src={facebookLogo} alt="Логотип Фэйсбук." />
+          <a className="footer__logo-link opacity tab tap-highlight" href="https://www.facebook.com/" target="_blank">
+            <img className="footer__logo footer__logo_type_facebook" src={facebookLogo} alt="Логотип Фэйсбук." />
           </a>
         </div>
       </nav>

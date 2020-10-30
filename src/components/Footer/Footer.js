d2c44a5785d160729f332bf67fdb8156c6bd36fd
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-10-30 19:05:51 +0300

    feat: сверстать footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,14 +10,14 @@ function Footer() {
       <p className="footer__copyright">&copy;&nbsp;2020 NewsExplorer, Powered by News API</p>
       <nav className="footer__navigation">
         <div>
-          <Link to="/" className="footer__link opacity tab tap-highlight">Главная</Link>
-          <a className="footer__link opacity tab tap-highlight" href="https://praktikum.yandex.ru" target="_blank">Яндекс.Практикум</a>
+          <Link to="/" className="footer__link opacity tap-highlight">Главная</Link>
+          <a className="footer__link opacity tap-highlight" href="https://praktikum.yandex.ru" target="_blank">Яндекс.Практикум</a>
         </div>
         <div>
-          <a className="footer__logo-link opacity tab tap-highlight" href="https://github.com/Vooooodoo" target="_blank">
+          <a className="footer__logo-link opacity tap-highlight" href="https://github.com/Vooooodoo" target="_blank">
             <img className="footer__logo footer__logo_type_github" src={githubLogo} alt="Логотип Гитхаб." />
           </a>
-          <a className="footer__logo-link opacity tab tap-highlight" href="https://www.facebook.com/" target="_blank">
+          <a className="footer__logo-link opacity tap-highlight" href="https://www.facebook.com/" target="_blank">
             <img className="footer__logo footer__logo_type_facebook" src={facebookLogo} alt="Логотип Фэйсбук." />
           </a>
         </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,13 +11,13 @@ function Navigation(props) {
       </Link>
       {props.loggedIn
         ? (<nav className="navigation__menu">
-            <Link to="/" className="navigation__link navigation__text opacity tab tap-highlight">Главная</Link>
-            <Link to="/saved-news" className="navigation__link navigation__text opacity tab tap-highlight">Сохранённые статьи</Link>
+            <Link to="/" className="navigation__link navigation__text opacity tap-highlight">Главная</Link>
+            <Link to="/saved-news" className="navigation__link navigation__text opacity tap-highlight">Сохранённые статьи</Link>
             <button className="navigation__desktop-button navigation__desktop-button_signout navigation__text opacity tab tap-highlight" type="button" onClick={props.onSignOut}>Грета</button>
           </nav>)
 
         : (<nav className="navigation__menu">
-            <Link to="/" className="navigation__link navigation__text opacity tab tap-highlight">Главная</Link>
+            <Link to="/" className="navigation__link navigation__text opacity tap-highlight">Главная</Link>
             <button className="navigation__desktop-button navigation__desktop-button_signin navigation__text opacity tab tap-highlight" type="button">Авторизоваться</button>
           </nav>)
       }

07c779624819c8e1a1b57589a0cf5334dccb87df
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-02 13:12:03 +0300

    feat: сверстать preloader

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,7 +6,17 @@ function Main(props) {
   return (
     <main>
       <Preloader />
-      <About />
+      <About
+        children={
+          <>
+            <img className="preloader__logo" src={preloaderLogo} alt="Лупа с грустным смайликом внутри." />
+            <h3 className="preloader__title">Ничего не&nbsp;найдено</h3>
+            <p className="preloader__subtitle">
+              К&nbsp;сожалению по&nbsp;вашему запросу ничего не&nbsp;найдено.
+            </p>
+          </>
+        }
+      />
     </main>
   );
 }

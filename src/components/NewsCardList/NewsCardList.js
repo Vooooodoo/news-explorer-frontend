9a34edf706543d4e81e7bc954bc7c63e111a26ac
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-03 17:38:18 +0300

    feat: реализовать секцию с новостными карточками

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -4,15 +4,13 @@ import './NewsCardList.css';
 
 function NewsCardList(props) {
   return (
-    <ul className="news-card-list"> {/* правила React требуют задавать ключи при работе со списками, они нужны для корректной работы движка */}
-      {props.cards.map((card) => (
-        (<NewsCard
-          card={card}
-          key={card._id}
-          onCardSave={props.onCardSave}
-          onCardDelete={props.onCardDelete}
-        />)
-      ))}
+    <ul className="news-card-list">
+      <NewsCard
+        card={card}
+        key={card._id}
+        onCardSave={props.onCardSave}
+        onCardDelete={props.onCardDelete}
+      />
     </ul>
   );
 }

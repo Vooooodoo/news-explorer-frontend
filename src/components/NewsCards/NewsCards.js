9c87de40829a84d2cb7d32d8ebc8b1cbde09c1a7
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-03 16:25:23 +0300

    feat: реализовать секцию с новостными карточками

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ShowMoreBtn from '../ShowMoreBtn/ShowMoreBtn';
 import './NewsCards.css';
 
 function NewsCards() {
   return (
     <section className="news-cards">
       <h2 className="news-cards__title">Результаты поиска</h2>
+      <ShowMoreBtn />
     </section>
   );
 }

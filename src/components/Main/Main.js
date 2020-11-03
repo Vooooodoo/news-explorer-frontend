5a2c3b43497f7328cc54bc211c383293e9b9d169
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-03 15:34:03 +0300

    feat: реализовать секцию с новостными карточками

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from '../About/About';
-import NotFound from '../NotFound/NotFound';
+import NewsCards from '../NewsCards/NewsCards';
 import Spinner from '../Spinner/Spinner';
+import NotFound from '../NotFound/NotFound';
 
 function Main(props) {
   return (
     <main>
+      <NewsCards />
       {/* <Spinner />
       <NotFound /> */}
       <About />
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NewsCards.css';
+
+function NewsCards() {
+  return (
+    <section className="news-cards">
+      <h2 className="news-cards__title">Результаты поиска</h2>
+    </section>
+  );
+}
+
+export default NewsCards;

c8d993685a21e073e3259e5472b4d60ba7090969
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-09 19:58:03 +0300

    feat: реализовать модальные окна

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -7,10 +7,14 @@ function PopupWithForm(props) {
       <form name={props.id.slice(0, props.id.indexOf('-'))} className="popup__form-container" onSubmit={props.onSubmit}>
         <h3 className="popup__form-title">{props.title}</h3>
         <fieldset className="popup__fieldset">
-          <input id="popup-email-input" name="email" className="popup__text-input" type="email" placeholder="Введите почту" minLength="2" maxLength="40" required />
+          <label for="popup-email-input" className="popup__input-label">Email</label>
+          <input id="popup-email-input" name="email" className="popup__input" type="email" placeholder="Введите почту" minLength="2" maxLength="40" required />
           <span id="popup-email-input-error" className="popup__input-error popup__input-error_hidden"></span>
-          <input id="popup-password-input" name="password" className="popup__text-input" type="password" placeholder="Введите пароль" minLength="8" maxLength="40" required />
+
+          <label for="popup-password-input" className="popup__input-label">Пароль</label>
+          <input id="popup-password-input" name="password" className="popup__input" type="password" placeholder="Введите пароль" minLength="8" maxLength="40" required />
           <span id="popup-password-input-error" className="popup__input-error popup__input-error_hidden"></span>
+
           {props.children}
         </fieldset>
         <button className="popup__submit popup__submit_valid tap-highlight" type="submit">{props.btnText}</button>

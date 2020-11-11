40e9badb88deed51caa5aa0813ed81efd759e0f4
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-11 17:53:35 +0300

    feat: реализовать модальные окна

--- src/components/LoginPopup/LoginPopup.js
+++ src/components/LoginPopup/LoginPopup.js
@@ -2,6 +2,77 @@ import React from 'react';
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 
 function LoginPopup(props) {
+  const errorsDefaultState = {
+    errorClass: '',
+    errorMessage: '',
+  };
+  const shownErrorClass = 'popup__input-error_shown';
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const [emailInputError, setEmailInputError] = React.useState(errorsDefaultState);
+  const [passwordInputError, setPasswordInputError] = React.useState(errorsDefaultState);
+
+  const [isEmailValid, setIsEmailValid] = React.useState(false);
+  const [isPasswordValid, setIsPasswordValid] = React.useState(false);
+  const [isFormValid, setIsFormValid] = React.useState(false);
+
+  function handleEmailChange(evt) {
+    if (!evt.target.validity.valid) {
+      setEmailInputError({
+        errorClass: shownErrorClass,
+        errorMessage: evt.target.validationMessage,
+      });
+      setIsEmailValid(false);
+    } else {
+      setEmailInputError(errorsDefaultState);
+      setIsEmailValid(true);
+    }
+  }
+
+  function handlePasswordChange(evt) {
+    if (!evt.target.validity.valid) {
+      setPasswordInputError({
+        errorClass: shownErrorClass,
+        errorMessage: evt.target.validationMessage,
+      });
+      setIsPasswordValid(false);
+    } else {
+      setPasswordInputError(errorsDefaultState);
+      setIsPasswordValid(true);
+    }
+  }
+
+  function resetInputErrors() {
+    setEmailInputError(errorsDefaultState);
+    setPasswordInputError(errorsDefaultState);
+  }
+
+  function resetFormFields() {
+    setEmail('');
+    setPassword('');
+  }
+
+  function resetValidationState() {
+    setIsEmailValid(false);
+    setIsPasswordValid(false);
+  }
+
+  React.useEffect(() => {
+    resetInputErrors();
+    resetFormFields();
+    resetValidationState();
+  }, [props.isOpen]);
+
+  React.useEffect(() => {
+    if (isEmailValid && isPasswordValid) {
+      setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
+    }
+  }, [isEmailValid, isPasswordValid]);
+
   return (
     <PopupWithForm
       id="login-popup"
@@ -9,9 +80,18 @@ function LoginPopup(props) {
       btnText="Войти"
       ariaLabel="Закрыть форму входа."
       linkText="Зарегистрироваться"
+      email={email}
+      password={password}
+      emailInputErrClass={emailInputError.errorClass}
+      emailInputErrMessage={emailInputError.errorMessage}
+      passInputErrClass={passwordInputError.errorClass}
+      passInputErrMessage={passwordInputError.errorMessage}
       isOpen={props.isOpen}
+      isFormValid={isFormValid}
       onClose={props.onClose}
       onClick={props.onClick}
+      onEmailChange={handleEmailChange}
+      onPassChange={handlePasswordChange}
     />
   );
 }
--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -2,119 +2,29 @@ import React from 'react';
 import './PopupWithForm.css';
 
 function PopupWithForm(props) {
-  const errorsDefaultState = {
-    errorClass: '',
-    errorMessage: '',
-  };
-  const shownErrorClass = 'popup__input-error_shown';
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [name, setName] = React.useState('');
-
-  const [emailInputError, setEmailInputError] = React.useState(errorsDefaultState);
-  const [passwordInputError, setPasswordInputError] = React.useState(errorsDefaultState);
-  const [nameInputError, setNameInputError] = React.useState(errorsDefaultState);
-
-  const [isEmailValid, setIsEmailValid] = React.useState(false);
-  const [isPasswordValid, setIsPasswordValid] = React.useState(false);
-  const [isNameValid, setIsNameValid] = React.useState(false);
-  const [isFormValid, setIsFormValid] = React.useState(false);
-
-  function handleEmailChange(evt) {
-    if (!evt.target.validity.valid) {
-      setEmailInputError({
-        errorClass: shownErrorClass,
-        errorMessage: evt.target.validationMessage,
-      });
-      setIsEmailValid(false);
-    } else {
-      setEmailInputError(errorsDefaultState);
-      setIsEmailValid(true);
-    }
-  }
-
-  function handlePasswordChange(evt) {
-    if (!evt.target.validity.valid) {
-      setPasswordInputError({
-        errorClass: shownErrorClass,
-        errorMessage: evt.target.validationMessage,
-      });
-      setIsPasswordValid(false);
-    } else {
-      setPasswordInputError(errorsDefaultState);
-      setIsPasswordValid(true);
-    }
-  }
-
-  function handleNameChange(evt) {
-    if (!evt.target.validity.valid) {
-      setNameInputError({
-        errorClass: shownErrorClass,
-        errorMessage: evt.target.validationMessage,
-      });
-      setIsNameValid(false);
-    } else {
-      setNameInputError(errorsDefaultState);
-      setIsNameValid(true);
-    }
-  }
-
-  function resetInputErrors() {
-    setEmailInputError(errorsDefaultState);
-    setPasswordInputError(errorsDefaultState);
-    setNameInputError(errorsDefaultState);
-  }
-
-  function resetFormFields() {
-    setEmail('');
-    setPassword('');
-    setName('');
-  }
-
-  function resetValidationState() {
-    setIsEmailValid(false);
-    setIsPasswordValid(false);
-    setIsNameValid(false);
-  }
-
-  React.useEffect(() => {
-    resetInputErrors();
-    resetFormFields();
-    resetValidationState();
-  }, [props.isOpen]);
-
-  React.useEffect(() => {
-    if (isEmailValid && isPasswordValid && isNameValid) {
-      setIsFormValid(true);
-    } else {
-      setIsFormValid(false);
-    }
-  }, [isEmailValid, isPasswordValid, isNameValid]);
-
   return (
     <div id={props.id} className={`popup ${props.isOpen ? 'popup_opened' : 'popup_closed'}`}>
       <form name={props.id.slice(0, props.id.indexOf('-'))} className="popup__form-container" onSubmit={props.onSubmit} noValidate>
         <h3 className="popup__form-title">{props.title}</h3>
         <fieldset className="popup__fieldset">
           <label for="popup-email-input" className="popup__input-label">Email</label>
-          <input id="popup-email-input" name="email" className="popup__input" onChange={handleEmailChange} defaultValue={email} type="email" placeholder="Введите почту" minLength="2" maxLength="40" required />
-          <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${emailInputError.errorClass}`}>{emailInputError.errorMessage}</span>
+          <input id="popup-email-input" name="email" className="popup__input" onChange={props.onEmailChange} value={props.email} type="email" placeholder="Введите почту" minLength="2" maxLength="40" required />
+          <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${props.emailInputErrClass}`}>{props.emailInputErrMessage}</span>
 
           <label for="popup-password-input" className="popup__input-label">Пароль</label>
-          <input id="popup-password-input" name="password" className="popup__input" onChange={handlePasswordChange} defaultValue={password} type="password" placeholder="Введите пароль" minLength="8" maxLength="40" required />
-          <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${passwordInputError.errorClass}`}>{passwordInputError.errorMessage}</span>
+          <input id="popup-password-input" name="password" className="popup__input" onChange={props.onPassChange} value={props.password} type="password" placeholder="Введите пароль" minLength="8" maxLength="40" required />
+          <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${props.passInputErrClass}`}>{props.passInputErrMessage}</span>
 
           {props.id === 'registration-popup'
             && (<>
                 <label for="popup-name-input" className="popup__input-label">Имя</label>
-                <input id="popup-name-input" name="name" className="popup__input" onChange={handleNameChange} defaultValue={name} type="text" placeholder="Введите своё имя" minLength="2" maxLength="40" pattern="^[А-Яа-яЁёa-zA-Z\s\-]+$" required />
-                <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${nameInputError.errorClass}`}>{nameInputError.errorMessage}</span>
+                <input id="popup-name-input" name="name" className="popup__input" onChange={props.onNameChange} value={props.name} type="text" placeholder="Введите своё имя" minLength="2" maxLength="40" pattern="^[А-Яа-яЁёa-zA-Z\s\-]+$" required />
+                <span className={`popup__input-error popup__input-error_type_validation popup__input-error_hidden ${props.nameInputErrClass}`}>{props.nameInputErrorMessage}</span>
               </>)
           }
           <span className="popup__input-error popup__input-error_type_registration popup__input-error_hidden">Такой пользователь уже есть</span>
         </fieldset>
-        <button className={`popup__submit tap-highlight ${isFormValid ? 'popup__submit_valid' : 'popup__submit_invalid'}`} type="submit">{props.btnText}</button>
+        <button className={`popup__submit tap-highlight ${props.isFormValid ? 'popup__submit_valid' : 'popup__submit_invalid'}`} type="submit">{props.btnText}</button>
         <button className="popup__close opacity tab tap-highlight" type="button" aria-label={props.ariaLabel} onClick={props.onClose}></button>
         <div className="popup__wrapper">
           <span className="popup__text">или</span>

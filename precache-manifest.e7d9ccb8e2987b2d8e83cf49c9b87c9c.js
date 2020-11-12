01ec9ac6e641a2ca5c5d633a01f357f3ec911858
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-12 11:29:34 +0300

    Updates

--- precache-manifest.e7d9ccb8e2987b2d8e83cf49c9b87c9c.js
+++ precache-manifest.e7d9ccb8e2987b2d8e83cf49c9b87c9c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "39e4233f76ea89ed293c56f507df52be",
+    "revision": "9764c6150efa54f817eb11ca955d4041",
     "url": "./index.html"
   },
   {
-    "revision": "c6b33310b1a7f2743b46",
+    "revision": "fbed91bb6d47507ceaa4",
     "url": "./static/css/main.ab0e2f58.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8760ffe0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c6b33310b1a7f2743b46",
-    "url": "./static/js/main.dbe5e12d.chunk.js"
+    "revision": "fbed91bb6d47507ceaa4",
+    "url": "./static/js/main.585504e1.chunk.js"
   },
   {
     "revision": "da153df23278d46de5d2",

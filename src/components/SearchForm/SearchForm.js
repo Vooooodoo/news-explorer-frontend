91059b702772a791bb69b96f0a0669d9726de35a
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-12 19:41:18 +0300

    fix: исправить ошибки

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import './SearchForm.css';
 function SearchForm() {
   return (
     <form className="search-form">
-      <input className="search-form__input" placeholder="Введите тему новости" />
+      <input className="search-form__input" placeholder="Введите тему новости" required />
       <button className="search-form__button search-form__text tap-highlight" type="submit">Искать</button>
     </form>
   );

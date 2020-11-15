44f6d73e83e2b95a6d858f5161daff9a1639f032
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-15 18:43:48 +0300

    feat: реализовать логику работы с NewsAPI

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,6 +1,6 @@
 class NewsApi {
   constructor(daysAgo) {
-    this._date = new Date();
+    this._date = new Date(); //* объект с текущей датой
     this._today = this._getDate(this._date);
     this._daysAgoDate = this._setDate(daysAgo);
     this._daysAgo = this._getDate(this._daysAgoDate);
@@ -12,11 +12,11 @@ class NewsApi {
     date.setDate(date.getDate() - daysAgo);
 
     return date;
-  }
+  } //* метод для установки даты n-ое количество дней назад, в зависимости от переданного аргумента
 
   _getDate(date) {
     return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-  }
+  } //* метод для получения даты в формате параметров запроса сервиса NewsAPI
 
   _fetch(theme, options) {
     return fetch(`https://newsapi.org/v2/everything?q=${theme}&from=${this._daysAgo}&to=${this._today}&language=ru&pageSize=100&apiKey=70091bdd11724ad58795297088c46456`, options)

b6e5ee4c32df0d22f4e471ab5025f40b1a16ddfa
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-16 11:42:48 +0300

    feat: реализовать логику работы с NewsAPI

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -5,11 +5,13 @@ import { ArticlesContext } from '../../contexts/ArticlesContext';
 
 function NewsCardList() {
   const articles = React.useContext(ArticlesContext);
+  let reactKey = 0;
 
   return (
     <ul className="news-card-list">
       {articles.map((article) => (
         (<NewsCard
+          key={reactKey++}
           photo={article.urlToImage}
           tagText="Природа"
           date={article.publishedAt}

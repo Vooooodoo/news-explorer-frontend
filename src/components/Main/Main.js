c662970995dfa92d488b17fc044ca9c57cf86397
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-16 12:01:37 +0300

    feat: реализовать логику работы с NewsAPI

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,15 +7,9 @@ import NotFound from '../NotFound/NotFound';
 function Main(props) {
   return (
     <main>
-      {props.isLoading
-        && <Spinner />
-      }
-      {props.isLoaded
-        && <SearchResult />
-      }
-      {props.isNotFound
-        && <NotFound />
-      }
+      {props.isLoading && <Spinner />}
+      {props.isLoaded && <SearchResult />}
+      {props.isNotFound && <NotFound />}
       <About />
     </main>
   );

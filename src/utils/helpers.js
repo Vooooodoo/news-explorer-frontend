bba4ead8374984fb198731999109a92facc0632e
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-19 23:32:35 +0300

    feat: реализовать личный кабинет пользователя

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,7 @@
-export const declOfNum = (n, titles) => titles[(n % 10 === 1 && n % 100 !== 11) ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+export function declOfNum(number, titles) {
+  return titles[(number % 10 === 1 && number % 100 !== 11)
+    ? 0
+    : number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20)
+      ? 1
+      : 2];
+}

27c5e9d7e3bdd93309137cb8c06a507d72a6b946
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-20 00:21:23 +0300

    feat: реализовать личный кабинет пользователя

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -30,14 +30,22 @@ function SavedNewsHeader(props) {
     return item.keyword;
   });
 
-  const articleName = declOfNum(savedArticles.length, ['статья', 'статьи', 'статей']);
-  const savedName = declOfNum(savedArticles.length, ['сохраненная', 'сохраненных', 'сохраненных']);
-  const otherTags = sortedKeywords.slice(3).length === 0 ? '' : declOfNum(sortedKeywords.slice(3).length, ['другому', 'другим', 'другим']);
+  const articleDecl = declOfNum(savedArticles.length, ['статья', 'статьи', 'статей']);
+  const savedDecl = declOfNum(savedArticles.length, ['сохраненная', 'сохраненных', 'сохраненных']);
+  const otherDecl =
+    sortedKeywords.slice(3).length === 0
+    ? ''
+    : declOfNum(sortedKeywords.slice(3).length, ['другому', 'другим', 'другим']);
   const topThreeKeywords = sortedKeywords.slice(0, 3).join(', ');
   const topTwoKeywords = sortedKeywords.slice(0, 2).join(', ');
-  const allKeywords = sortedKeywords.length <= 3 ? topThreeKeywords : `${topTwoKeywords} и ${sortedKeywords.slice(3).length}`;
-  const keywordPostfix = sortedKeywords.slice(3).length > 4 || sortedKeywords.slice(3).length < 2 ? '' : '-м';
-  console.log(topTwoKeywords);
+  const allKeywords =
+    sortedKeywords.length <= 3
+    ? topThreeKeywords
+    : `${topTwoKeywords} и ${sortedKeywords.slice(3).length}`;
+  const keywordPostfix =
+    sortedKeywords.slice(3).length > 4 || sortedKeywords.slice(3).length < 2
+    ? ''
+    : '-м';
 
   return (
     <header className="saved-news-header">
@@ -46,8 +54,8 @@ function SavedNewsHeader(props) {
         onSignOut={props.onSignOut}
       />
       <h1 className="saved-news-header__title">Сохранённые статьи</h1>
-      <p className="saved-news-header__subtitle">{`${currentUser.name}, у вас ${props.articles.length} ${savedName} ${articleName}`}</p>
-      <p className="saved-news-header__text">По&nbsp;ключевым словам: <b>{`${allKeywords}${keywordPostfix} ${otherTags}`}</b></p>
+      <p className="saved-news-header__subtitle">{`${currentUser.name}, у вас ${props.articles.length} ${savedDecl} ${articleDecl}`}</p>
+      <p className="saved-news-header__text">По&nbsp;ключевым словам: <b>{`${allKeywords}${keywordPostfix} ${otherDecl}`}</b></p>
     </header>
   );
 }

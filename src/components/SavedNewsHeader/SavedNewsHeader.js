188d0e36e94f2c2f57adc03d08efe8a545b2e9b7
Roman Andrukhanenko <vooooodoo@yandex.ru>
2020-11-19 14:38:38 +0300

    feat: реализовать личный кабинет пользователя

--- src/components/SavedNewsHeader/SavedNewsHeader.js
+++ src/components/SavedNewsHeader/SavedNewsHeader.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import Navigation from '../Navigation/Navigation';
 import './SavedNewsHeader.css';
 
 function SavedNewsHeader(props) {
+  const currentUser = React.useContext(CurrentUserContext);
+
   return (
     <header className="saved-news-header">
       <Navigation
@@ -10,7 +13,7 @@ function SavedNewsHeader(props) {
         onSignOut={props.onSignOut}
       />
       <h1 className="saved-news-header__title">Сохранённые статьи</h1>
-      <p className="saved-news-header__subtitle">Грета, у&nbsp;вас 5&nbsp;сохранённых статей</p>
+      <p className="saved-news-header__subtitle">{`${currentUser.name}, у вас ${props.articles.length} сохранённых статей`}</p>
       <p className="saved-news-header__text">По&nbsp;ключевым словам: <b>Природа</b>, <b>Тайга</b> и&nbsp;<b>2-м другим</b></p>
     </header>
   );
